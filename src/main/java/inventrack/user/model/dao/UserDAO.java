732d4140193852700096fd51cf14e067712d5ace
anmol123232 <anmolniraula@Anmols-MacBook-Pro.local>
2026-04-20 09:26:42 +0545

    register user added

--- src/main/java/inventrack/user/model/dao/UserDAO.java
+++ src/main/java/inventrack/user/model/dao/UserDAO.java
@@ -5,7 +5,23 @@ import inventrack.user.model.User;
 public class UserDAO implements UserInterface{
     @Override
     public boolean registerUser(User user) {
-        return false;
+        if(user.getEmail().trim().isEmpty() || user.getPassword().trim().isEmpty()){
+            return false;
+        }
+        String sql = "INSER INTO user(name,email,password,role) values(?,?,?,?)";
+        try(Connection conn = DBConnection.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+        ) {
+            ps.setString(1,user.getName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3,user.getPassword());
+            ps.setString(4,user.getRole());
+            int row = ps.executeUpdate();
+            return row > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
