8ceec567ad29a796b4209b2e83328e5258bcfa44
8972-amick <khadkaamick2@gmail.com>
2026-04-27 22:10:43 +0545

    Database setup for the invenTrack

--- src/main/java/inventrack/utils/DBConnection.java
+++ src/main/java/inventrack/utils/DBConnection.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/testdb";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/inventrack_db";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "@1234";
 
@@ -13,3 +13,4 @@ public class DBConnection {
         return DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
     }
 }
+
